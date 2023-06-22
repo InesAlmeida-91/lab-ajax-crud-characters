0aff38486aa8d8f4391af884046058ef2ab42537
Inês Almeida <ines_almeida_203@hotmail.com>
2023-06-22 17:02:49 +0100

    missing iteration 3

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,58 @@
+const axios = require('axios');
+
+
 class APIHandler {
   constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+    this.BASE_URL = axios.create({
+      baseURL: baseUrl,
+    });
   }
 
-  getFullList () {
+  async getFullList () {
+    try {
+      const response = await this.BASE_URL.get('/characters');
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  } 
 
+  async getOneRegister (characterId) {
+    try {
+      const response = await this.BASE_URL.get(`/characters/${characterId}`);
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  } 
+
+  async createOneRegister (characterCreated) {
+    try {
+      const response = await this.BASE_URL.post('/characters', characterCreated);
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
   }
 
-  getOneRegister () {
-
+  async updateOneRegister (characterId, updatedCharacter) {
+    try {
+      const response = await this.BASE_URL.put(`/characters/${characterId}`, updatedCharacter )
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
   }
 
-  createOneRegister () {
-
+  async deleteOneRegister(characterId) {
+    try {
+      await this.BASE_URL.delete(`/characters/${characterId}`);
+      return 'Character has been successfully deleted';
+    } catch (error) {
+      console.error(error);
+      }
+    }
   }
 
-  updateOneRegister () {
 
-  }
-
-  deleteOneRegister () {
-
-  }
-}
+module.exports = APIHandler;
